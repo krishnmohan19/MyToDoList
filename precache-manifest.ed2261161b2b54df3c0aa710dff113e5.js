fc97ed713306656e9e2699f45daadfa04cffd25d
krishnmohan <krishnmohan2819@gmail.com>
2020-12-07 15:35:39 +0530

    Updates

---
+++ precache-manifest.ed2261161b2b54df3c0aa710dff113e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969aad779e2cf79f217c1d4ebc296869",
+    "url": "/MyToDoList/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/MyToDoList/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "6c0f612faa2528243844",
+    "url": "/MyToDoList/static/js/main.3ced063b.chunk.js"
+  },
+  {
+    "revision": "6373c650b3e65aefb251",
+    "url": "/MyToDoList/static/js/runtime-main.a1e91b8c.js"
+  }
+]);
